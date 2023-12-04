5d98e9429fb5e706f648e9b86d4e284b49ddea40
andreyrcdias <andreycadimaa@gmail.com>
2023-12-03 22:20:21 -0300

    feat: day 1 part 2 ✓

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,43 +1,70 @@
-use regex::Regex;
+//use regex::Regex;
 use std::fs::read_to_string;
 
-fn part1(lines: &Vec<&str>) -> u32 {
-    // lines.iter().map(|line| {
-    //     let first = line.chars().find_map(|c| c.to_digit(10)).unwrap();
-    //     let last = line.chars().rev().find_map(|c| c.to_digit(10)).unwrap();
-    //     first * 10 + last
-    // }).sum();
-
-    // approach using regex
-    let digit_pattern = Regex::new(r"\d").unwrap();
+fn part1(input: &str) -> u32 {
+    let lines: Vec<&str> = input.lines().collect();
     lines
         .iter()
         .map(|line| {
-            let first = digit_pattern
-                .find(line)
-                .unwrap()
-                .as_str()
-                .parse::<u32>()
-                .unwrap();
-            let last = digit_pattern
-                .find(&line.chars().rev().collect::<String>())
-                .unwrap()
-                .as_str()
-                .parse::<u32>()
-                .unwrap();
+            let first = line.chars().find_map(|c| c.to_digit(10)).unwrap();
+            let last = line.chars().rev().find_map(|c| c.to_digit(10)).unwrap();
             first * 10 + last
+        })
+        .sum()
+
+    // approach using regex
+    // let digit_pattern = Regex::new(r"\d").unwrap();
+    // lines
+    //     .iter()
+    //     .map(|line| {
+    //         let first = digit_pattern
+    //             .find(line)
+    //             .unwrap()
+    //             .as_str()
+    //             .parse::<u32>()
+    //             .unwrap();
+    //         let last = digit_pattern
+    //             .find(&line.chars().rev().collect::<String>())
+    //             .unwrap()
+    //             .as_str()
+    //             .parse::<u32>()
+    //             .unwrap();
+    //         first * 10 + last
+    //     })
+    //     .sum()
+}
+
+fn part2(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            line.to_string()
+                .replace("one", "one1one")
+                .replace("two", "two2twooo")
+                .replace("three", "three3three")
+                .replace("four", "four4four")
+                .replace("five", "five5five")
+                .replace("six", "six6six")
+                .replace("seven", "seven7seven")
+                .replace("eight", "eight8eight")
+                .replace("nine", "nine9nine")
+        })
+        .map(|line| {
+            line.chars()
+                .filter_map(|c| c.to_digit(10))
+                .collect::<Vec<u32>>()
+        })
+        .map(|vec| {
+            let first = vec.first().unwrap();
+            let last = vec.last().unwrap();
+            10 * first + last
         })
         .sum()
 }
 
-fn part2(_lines: &Vec<&str>) -> u32 {
-    todo!();
-}
-
 fn main() {
     const FILE_PATH: &'static str = "input.txt";
-    let content = read_to_string(FILE_PATH).expect(&format!("Unable to read {}", FILE_PATH));
-    let lines: Vec<&str> = content.lines().collect();
-    println!("part 1 = {:?}", part1(&lines));
-    // println!("part 2 = {:?}", part2(&lines));
+    let input = read_to_string(FILE_PATH).expect(&format!("Unable to read {}", FILE_PATH));
+    println!("part 1 = {:?}", part1(&input));
+    println!("part 2 = {:?}", part2(&input));
 }
