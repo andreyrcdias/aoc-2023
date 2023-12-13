08d8540c4477c6e3fe5b6755b14f2d710fc30165
andreyrcdias <andreycadimaa@gmail.com>
2023-12-13 08:19:45 -0300

    chore: day3 part1 code cleanup

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -6,14 +6,13 @@ fn part1(input: &str) -> u32 {
     let lines: Vec<&str> = input.lines().collect();
 
     let symbol_regex = Regex::new(r"[^.\d]").unwrap();
-
-    let mut symbol_adjacent: HashSet<(usize, usize)> = HashSet::new();
+    let mut adjacent_symbols: HashSet<(usize, usize)> = HashSet::new();
     for (i, line) in lines.iter().enumerate() {
         for m in symbol_regex.find_iter(line) {
             let j = m.start();
             for row in i.saturating_sub(1)..=i + 1 {
                 for c in j.saturating_sub(1)..=j + 1 {
-                    symbol_adjacent.insert((row, c));
+                    adjacent_symbols.insert((row, c));
                 }
             }
         }
@@ -24,7 +23,7 @@ fn part1(input: &str) -> u32 {
     for (i, line) in lines.iter().enumerate() {
         for m in number_regex.find_iter(line) {
             let (start, end) = (m.start(), m.end());
-            if (start..end).any(|j| symbol_adjacent.contains(&(i, j))) {
+            if (start..end).any(|j| adjacent_symbols.contains(&(i, j))) {
                 sum += m.as_str().parse::<u32>().unwrap();
             }
         }
