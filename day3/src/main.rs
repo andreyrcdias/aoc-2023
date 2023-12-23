76307c91121b93a0eb3811e0f707ffbf42bc3ec8
andreyrcdias <andreycadimaa@gmail.com>
2023-12-23 10:45:47 -0300

    feat: day 3 inspired by uncle scientist resolution

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,39 +1,135 @@
-use regex::Regex;
 use std::collections::HashSet;
-use std::fs::read_to_string;
+use std::fs;
 
-fn part1(input: &str) -> u32 {
-    let lines: Vec<&str> = input.lines().collect();
+struct PartNumber {
+    value: i64,
+    points: HashSet<(i64, i64)>,
+}
 
-    let symbol_regex = Regex::new(r"[^.\d]").unwrap();
-    let mut adjacent_symbols: HashSet<(usize, usize)> = HashSet::new();
-    for (i, line) in lines.iter().enumerate() {
-        for m in symbol_regex.find_iter(line) {
-            let j = m.start();
-            for row in i.saturating_sub(1)..=i + 1 {
-                for c in j.saturating_sub(1)..=j + 1 {
-                    adjacent_symbols.insert((row, c));
+impl PartNumber {
+    fn new(row: i64, col: i64, ch: char) -> Self {
+        let points = HashSet::from([
+            // right
+            (row, col + 1),
+            (row + 1, col + 1),
+            // above + below
+            (row - 1, col),
+            (row + 1, col),
+            (row - 1, col + 1),
+            // left
+            (row - 1, col - 1),
+            (row, col - 1),
+            (row + 1, col - 1),
+        ]);
+        Self {
+            value: (ch as u8 - b'0') as i64,
+            points,
+        }
+    }
+
+    fn add_digit(&mut self, row: i64, col: i64, ch: char) {
+        self.value = self.value * 10 + (ch as u8 - b'0') as i64;
+        self.points
+            .extend([(row - 1, col + 1), (row, col + 1), (row + 1, col + 1)]);
+    }
+
+    fn extract_value(&self) -> i64 {
+        self.value
+    }
+
+    fn next_to_symbol(&self, symbols: &HashSet<(i64, i64)>) -> bool {
+        self.points.intersection(symbols).next().is_some()
+    }
+}
+
+struct Data {
+    numbers: Vec<PartNumber>,
+    symbols: HashSet<(i64, i64)>,
+    gears: HashSet<(i64, i64)>,
+}
+
+impl Data {
+    pub fn new(
+        numbers: Vec<PartNumber>,
+        symbols: HashSet<(i64, i64)>,
+        gears: HashSet<(i64, i64)>,
+    ) -> Self {
+        Self {
+            numbers,
+            symbols,
+            gears,
+        }
+    }
+}
+
+fn parse_input(input: &str) -> Data {
+    let lines: Vec<&str> = input.lines().filter(|&s| !s.trim().is_empty()).collect();
+
+    let mut numbers: Vec<PartNumber> = Vec::new();
+    let mut symbols: HashSet<(i64, i64)> = HashSet::new();
+    let mut gears: HashSet<(i64, i64)> = HashSet::new();
+
+    let mut cur_number: Option<PartNumber> = None;
+
+    for (row, line) in lines.iter().enumerate() {
+        for (col, ch) in line.chars().enumerate() {
+            if ch.is_ascii_digit() {
+                if let Some(ref mut num) = cur_number {
+                    num.add_digit(row as i64, col as i64, ch);
+                } else {
+                    cur_number = Some(PartNumber::new(row as i64, col as i64, ch));
+                }
+            } else {
+                if let Some(num) = cur_number.take() {
+                    numbers.push(num);
+                }
+                if ch != '.' {
+                    symbols.insert((row as i64, col as i64));
+                    if ch == '*' {
+                        gears.insert((row as i64, col as i64));
+                    }
                 }
             }
         }
     }
 
-    let mut sum = 0;
-    let number_regex = Regex::new(r"\d+").unwrap();
-    for (i, line) in lines.iter().enumerate() {
-        for m in number_regex.find_iter(line) {
-            let (start, end) = (m.start(), m.end());
-            if (start..end).any(|j| adjacent_symbols.contains(&(i, j))) {
-                sum += m.as_str().parse::<u32>().unwrap();
+    Data::new(numbers, symbols, gears)
+}
+
+fn part1(input: &str) -> i64 {
+    let data = parse_input(input);
+    let total = data
+        .numbers
+        .iter()
+        .filter(|num| num.next_to_symbol(&data.symbols))
+        .map(PartNumber::extract_value)
+        .sum::<i64>();
+    total
+}
+
+fn part2(input: &str) -> i64 {
+    let data = parse_input(input);
+    let mut total = 0;
+    'next_gear: for gear in &data.gears {
+        let mut matches = Vec::new();
+        for num in &data.numbers {
+            if num.points.contains(gear) {
+                if matches.len() == 2 {
+                    continue 'next_gear;
+                }
+                matches.push(num.value);
             }
         }
+        if matches.len() == 2 {
+            total += matches[0] * matches[1];
+        }
     }
-    sum
+    total
 }
 
 fn main() {
     const FILE_PATH: &str = "input.txt";
-    let input =
-        read_to_string(FILE_PATH).unwrap_or_else(|_| panic!("Unable to read {}", FILE_PATH));
+    let input = fs::read_to_string(FILE_PATH).expect("Unable to open file");
     println!("part 1 = {:?}", part1(&input));
+    println!("part 2 = {:?}", part2(&input));
 }
