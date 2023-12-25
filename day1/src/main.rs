f53ad453f905329ba7f8aad402279b62c11f7486
andreyrcdias <andreycadimaa@gmail.com>
2023-12-25 10:06:06 -0300

    day 1 part 2 using aho-corasick algorithm

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,8 @@
 //use regex::Regex;
 use std::fs;
 
+use aho_corasick::AhoCorasick;
+
 fn part1(input: &str) -> u32 {
     let lines: Vec<&str> = input.lines().collect();
     lines
@@ -60,14 +62,42 @@ fn part2(input: &str) -> u32 {
             10 * first + last
         })
         .sum()
-    // TODO: test approach using Aho-Corasick algorithm
 }
+
+fn part2_aho_corasick(input: &str) -> i32 {
+    let nums = [
+        "one", "1",
+        "two", "2",
+        "three", "3",
+        "four", "4",
+        "five", "5",
+        "six", "6",
+        "seven", "7",
+        "eight", "8",
+        "nine", "9",
+    ];
+
+    let ac = AhoCorasick::new(nums).unwrap();
+
+    let total = input.lines()
+        .map(|line| {
+            let matches = ac.find_overlapping_iter(line).collect::<Vec<_>>();
+            let first = matches.first().unwrap().pattern().as_i32() / 2 + 1;
+            let last = matches.last().unwrap().pattern().as_i32() / 2 + 1;
+            10 * first + last
+        })
+        .sum();
+
+    total
+}
+
 
 fn main() {
     const FILE_PATH: &str = "input.txt";
     let input = fs::read_to_string(FILE_PATH).expect("Unable to open file");
     println!("part 1 = {:?}", part1(&input));
     println!("part 2 = {:?}", part2(&input));
+    println!("part 2 AHO Corasick = {:?}", part2_aho_corasick(&input));
 }
 
 #[cfg(test)]
@@ -86,5 +116,12 @@ mod tests {
         const FILE_PATH: &str = "sample2.txt";
         let input = fs::read_to_string(FILE_PATH).expect("Unable to open file");
         assert_eq!(part2(&input), 281);
+    }
+
+    #[test]
+    fn test_part2_aho_corasick() {
+        const FILE_PATH: &str = "sample2.txt";
+        let input = fs::read_to_string(FILE_PATH).expect("Unable to open file");
+        assert_eq!(part2_aho_corasick(&input), 281);
     }
 }
